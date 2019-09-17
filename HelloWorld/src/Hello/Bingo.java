011e608a0af845ffe31108f1298a27f8140e16e4
polarkuma <polarkuma@DESKTOP-QAV75D2>
2019-09-17 22:03:19 +0900

    hello

--- HelloWorld/src/Hello/Bingo.java
+++ HelloWorld/src/Hello/Bingo.java
@@ -1,23 +1,21 @@
 package Hello;
 
-// import java.util.Scanner;
-
 public class Bingo {
 	public static void main(String[] args) {
-//		Scanner sc = new Scanner(System.in);
+
 		int[][] aAry = new int[5][5]; // =
 										// {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
 		int c = 1;
 		for (int a = 0; a < aAry.length; a++) {
-			for (int b = 0; b < aAry[b].length; b++) {
+			for (int b = 0; b < aAry.length; b++) {
 				aAry[a][b] = c++;
 			}
 		}
 //		for (int i = 0; i < aAry.length; i++) {
 //			for (int j = 0; j < aAry.length; j++) {
-		for (int i = 0; i < aAry.length; i++) {
-			for (int j = 0; j < aAry.length; j++) {
-				System.out.print(aAry[i][j]);
+		for (int j = 4; j >= 0; j--) {
+			for (int i = 0; i <= 4; i++) {
+				System.out.print(aAry[i][j] + " ");
 			}
 			System.out.println();
 		}
